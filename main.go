939db3a65285543edcee5759dc568d21f8983c38
scriptonist <aravindkp@outlook.in>
2017-12-23 16:21:28 +0530

    Attempting to isolate PID's, Yeee Half baked !

--- main.go
+++ main.go
@@ -11,21 +11,32 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		fmt.Println("Ohh Ohh")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v %v \n", os.Args[1], os.Args[2:])
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	// Cloning a new hostname a.k.a UnixTimeSharing system(UTS)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v %v as PID %v \n", os.Args[1], os.Args[2:], os.Getppid())
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
-	// Cloning a new hostname aks UnixTimeSharing system(UTS)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
-	}
 
 	must(cmd.Run())
 }
